21c20498e40009b62f4e2e6e037b42b32bb864b3
Egy_ember <kamufiok06@gmail.com>
2026-06-25 00:55:36 +0200

    basics

---
+++ go.mod
@@ -0,0 +1,3 @@
+module functional-slices
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,27 @@
+package functionalslices
+
+func Filter[T any](s []T, cond func(T) bool) (r []T) {
+	r = make([]T, 0)
+	for _, v := range s {
+		if cond(v) {
+			r = append(r, v)
+		}
+	}
+	return
+}
+
+func Map[A any, B any](s []A, fn func(A) B) (r []B) {
+	r = make([]B, len(s))
+	for k, v := range s {
+		r[k] = fn(v)
+	}
+	return
+}
+
+func FoldR[A any, B any](start A, slice []B, fn func(A, B) A) (r A) {
+	r = start
+	for _, v := range slice {
+		r = fn(r, v)
+	}
+	return
+}
