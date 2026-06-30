62cf4711a50bb9ae639b9ed97b80dd2ebf993ed6
Egy_ember <kamufiok06@gmail.com>
2026-06-30 21:44:19 +0200

    added iterators and tests

--- main.go
+++ main.go
@@ -2,6 +2,8 @@
 // fontains public api and most implementations for functional slice operations like map and filter
 package functionalslices
 
+import "iter"
+
 func Filter[T any](s []T, cond func(T) bool) (r []T) {
 	r = make([]T, 0)
 	for _, v := range s {
@@ -45,4 +47,51 @@ func ParMap[A any, B any](s []A, fn func(A) B, threadlimit int) (r []B) {
 		<-tlch
 	}
 	return
+}
+
+func MapIter[A any, B any](s []A, fn func(A) B) iter.Seq[B] {
+	return func(yild func(B) bool) {
+		for _, v := range s {
+			if !yild(fn(v)) {
+				return
+			}
+		}
+	}
+}
+
+func MapIter2[A any, B any](s []A, fn func(A) B) iter.Seq2[int, B] {
+	return func(yild func(int, B) bool) {
+		for k, v := range s {
+			if !yild(k, fn(v)) {
+				return
+			}
+		}
+	}
+}
+
+func FilterIter[T any](s []T, cond func(T) bool) iter.Seq[T] {
+	return func(yild func(T) bool) {
+		for _, v := range s {
+			if !cond(v) {
+				continue
+			}
+			if !yild(v) {
+				return
+			}
+		}
+	}
+}
+
+// FilterIter2 teakes the key is the index from the original slice
+func FilterIter2[T any](s []T, cond func(T) bool) iter.Seq2[int, T] {
+	return func(yild func(int, T) bool) {
+		for k, v := range s {
+			if !cond(v) {
+				continue
+			}
+			if !yild(k, v) {
+				return
+			}
+		}
+	}
 }
--- main_test.go
+++ main_test.go
@@ -43,6 +43,21 @@ func TestMap(t *testing.T) {
 	}
 }
 
+func TestMap_order(t *testing.T) {
+	s := make([]int, 10)
+	for i := range 10 {
+		s[i] = i
+	}
+	r := Map(s, func(i int) int {
+		return i
+	})
+	for i := range 10 {
+		if r[i] != i {
+			t.Errorf("wrong value")
+		}
+	}
+}
+
 func TestParMap(t *testing.T) {
 	s := make([]int, 10)
 	for i := range 10 {
@@ -64,5 +79,89 @@ func TestParMap(t *testing.T) {
 		if r[i] != p[i] {
 			t.Errorf("wrong value")
 		}
+	}
+}
+
+func TestParMap_order(t *testing.T) {
+	s := make([]int, 10)
+	for i := range 10 {
+		s[i] = i
+	}
+	p := ParMap(s, func(i int) int {
+		return i
+	}, 4)
+	for i := range 10 {
+		if p[i] != i {
+			t.Errorf("wrong value")
+		}
+	}
+}
+
+func TestMapIter(t *testing.T) {
+	s := make([]int, 10)
+	for i := range 10 {
+		s[i] = 1
+	}
+	for i := range MapIter(s, func(i int) int {
+		if i == 1 {
+			return 2
+		}
+		return 3
+	}) {
+		if i != 2 {
+			t.Errorf("wrong value")
+		}
+	}
+}
+
+func TestMapIter_order(t *testing.T) {
+	s := make([]int, 10)
+	for i := range 10 {
+		s[i] = i
+	}
+	oldi := 0
+	for i := range MapIter(s, func(i int) int {
+		return i
+	}) {
+		if i != oldi {
+			t.Errorf("wrong value")
+		}
+		oldi++
+	}
+}
+
+func TestMapIter2(t *testing.T) {
+	s := make([]int, 10)
+	for i := range 10 {
+		s[i] = 1
+	}
+	for _, i := range MapIter2(s, func(i int) int {
+		if i == 1 {
+			return 2
+		}
+		return 3
+	}) {
+		if i != 2 {
+			t.Errorf("wrong value")
+		}
+	}
+}
+
+func TestMapIter2_order(t *testing.T) {
+	s := make([]int, 10)
+	for i := range 10 {
+		s[i] = i
+	}
+	oldi := 0
+	for k, i := range MapIter2(s, func(i int) int {
+		return i
+	}) {
+		if i != k {
+			t.Errorf("wrong value")
+		}
+		if k != oldi {
+			t.Errorf("wrong key")
+		}
+		oldi++
 	}
 }
